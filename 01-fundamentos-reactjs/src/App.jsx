4620ef4f64d168cd6f4be8ed649ce953bc7a5dc5
Pedro Priori <103284094+Pedro-Priori@users.noreply.github.com>
2025-03-17 20:47:10 -0300

    finalizando os motores do react

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -7,6 +7,35 @@ import styles from './app.module.css';
 
 import './Global.css';
 
+const posts = [{
+  id: 1, 
+  author:{
+    avatarUrl: "https://media.licdn.com/dms/image/v2/D4D03AQEAXy4QT3FM8w/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1699627425714?e=1747267200&v=beta&t=_jwT_MnaYrfXzY4cRCY9Qiaki0frl7-kv0fk1o4tH2k",
+    name: "Pedro Priori",
+    role: "Developer FullStack JR"
+  },
+  content: [
+    {type: "paragraph", content: "Fala galeraa 👋", },
+    {type: "paragraph", content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀"},
+    {type: "link", content:"jane.design/doctorcare"}
+  ],
+  publishedAt: new Date('2025-03-13 23:00:00')
+},
+
+  {id:2, 
+  author:{
+    avatarUrl: "https://media.licdn.com/dms/image/v2/D4D03AQF2eP3hc2DuFg/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1710771739127?e=1747267200&v=beta&t=kv0E5yRgbUYlLSKNgeRqVxx349jRJG0YHEGuTbtvDsQ",
+    name: "Maria Eugenia", 
+    role: "assistente de dados amarante"
+  },
+  content: [
+    {type: "paragraph", content: "Fala galeraa 👋", },
+    {type: "paragraph", content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀"},
+    {type: "link", content:"jane.design/doctorcare"}
+  ],
+  publishedAt: new Date('2025-03-13 20:10:00')
+}
+];
 
 export function App() {
   return (
@@ -18,8 +47,16 @@ export function App() {
           <Sidebar/>
         </aside>
         <main>
-          <Post/>
-          <Post/>
+          {posts.map(post =>{
+            return (
+              <Post
+                key={post.id}
+                author= {post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
--- 01-fundamentos-reactjs/src/components/Comment.jsx
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -3,7 +3,11 @@ import styles from './Comment.module.css'
 import { ThumbsUp } from '@phosphor-icons/react/dist/ssr'
 import { Avatar } from './Avatar'
 
-export function Comment(){
+export function Comment({ content , onDeleteComment}){
+    function handleDeleteComment(){
+        onDeleteComment(content) 
+    }
+
     return(
         <div className={styles.comment}>
             <Avatar hasBorder ={false} src="https://media.licdn.com/dms/image/v2/D4D03AQEAXy4QT3FM8w/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1699627425714?e=1747267200&v=beta&t=_jwT_MnaYrfXzY4cRCY9Qiaki0frl7-kv0fk1o4tH2k" alt="" />
@@ -15,13 +19,13 @@ export function Comment(){
                         <strong>Pedro Priori</strong>
                         <time title="10 de março de 2025 as 21 e 24" dateTime="2025-03-10  21:24:30">Cerca de 1h atrás </time>
                         </div>
-                        <button title='deletar comentario'> 
+                        <button onClick={handleDeleteComment} title='deletar comentario'> 
                             <Trash size={24}/>
                         </button>
                     </header>
-                    <p>
-                    Muito bom Devon, parabéns!! 👏👏
-                    </p>
+
+                    <p>{content}</p>
+                    
                 </div>
 
                 <footer>
